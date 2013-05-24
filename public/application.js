37c800bc5f23ea5029064f92e4b9099a8025b2de
sookcha <lsookcha@me.com>
2013-05-24 22:26:45 +0900

    Preparation to use javascript

--- public/application.js
+++ public/application.js
@@ -1,5 +1,20 @@
 (function($){
 	$(function(){
+		var postData = "pt=member&member_id_crypt=czIwMTMxNTEz&passwd_crypt=NzQzNnEmcQ=="
+			
+			$.ajax({
+			  url: "http://disk.dimigo.hs.kr:8282/LoginService.do"
+			, type: 'POST'
+			, contentType: 'application/x-www-form-urlencoded'
+			, data: postData
+			, success: function(data){ 
+				console.log(data);
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		});
+		
 		console.log("사이트 뜯어보지 마라!")
 	});
 }(Zepto))
