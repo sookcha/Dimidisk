ef318ebaf0091a064d1fb48127f45a89c64c8c15
sookcha <lsookcha@me.com>
2014-05-26 13:51:59 +0900

    Linguist calculation optimization

--- partial.rb
+++ partial.rb
@@ -4,7 +4,7 @@ end
 
 Sinatra::Base.get '/grid.css' do
   headers["Content-Type"] = "text/css"
-  send_file "public/grid.css"
+  send_file "vendor/grid.css"
 end
 
 Sinatra::Base.get '/intro.jpg' do
@@ -17,7 +17,7 @@ Sinatra::Base.get '/font.css' do
 end
 
 Sinatra::Base.get "/zepto-autocomplete.js" do
-  send_file "public/zepto-autocomplete.js"
+  send_file "vendor/zepto-autocomplete.js"
 end
 
 Sinatra::Base.get "/application.js" do
@@ -30,21 +30,21 @@ end
 
 
 Sinatra::Base.get "/jquery-1.10.2.min.js" do
-  send_file "public/jquery-1.10.2.min.js"
+  send_file "vendor/jquery-1.10.2.min.js"
 end
 
 Sinatra::Base.get "/nprogress.css" do
-  send_file "public/nprogress.css"
+  send_file "vendor/nprogress.css"
 end
 
 Sinatra::Base.get "/nprogress.js" do
-  send_file "public/nprogress.js"
+  send_file "vendor/nprogress.js"
 end
 
 Sinatra::Base.get "/dimidisk-less.mp4" do
-  send_file "public/dimidisk-less.mp4"
+  send_file "vendor/dimidisk-less.mp4"
 end
 
 Sinatra::Base.get "/turbolinks.js" do
-  send_file "public/turbolinks.js"
+  send_file "vendor/turbolinks.js"
 end
